cf2392079c94c7ccb34df57cdf0fd3aca8e04a33
rasa9521 <rsrasasingh@gmail.com>
2025-04-16 18:35:29 +0530

    Update main.py

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ cookies_file_path = os.getenv("COOKIES_FILE_PATH", "youtube_cookies.txt")
 
 #pwimg = "https://graph.org/file/8add8d382169e326f67e0-3bf38f92e52955e977.jpg"
 #ytimg = "https://graph.org/file/3aa806c302ceec62e6264-60ced740281395f68f.jpg"
-cpimg = "https://graph.org/file/5ed50675df0faf833efef-e102210eb72c1d5a17.jpg"  
+cpimg = "https://unitedcamps.in/Images/IMG_1744262656.jpg"  
 
 
 async def show_random_emojis(message):
